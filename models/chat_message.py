b90580c2a59e28d367e69b28d6f9ffd2c286d7ae
rokussadee <rokus.sadee@student.ehb.be>
2024-02-20 21:30:35 +0100

    chore(chatbot-models): refined models
    type hints, type constraints & default values

--- models/chat_message.py
+++ models/chat_message.py
@@ -2,8 +2,15 @@ from datetime import datetime
 
 
 class ChatMessage:
-    def __init__(self, author: str, content: str, chat_room: int):
-        self.created_time = datetime.now()
-        self.author = author
-        self.content = content
-        self.chat_room = chat_room
+    def __init__(self, author: str, content: str, chat_room_id: int):
+        """
+        Represents a message in a chat room.
+
+        :param author: The author of the message.
+        :param content: The content of the message.
+        :param chat_room_id: The ID of the chat room to which the message belongs (foreign key to ChatRoom).
+        """
+        self.created_time: datetime = datetime.now()
+        self.author: str = author
+        self.content:str = content
+        self.chat_room_id:int = chat_room_id
