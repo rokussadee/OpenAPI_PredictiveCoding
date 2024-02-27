deed05fe8d827ca7de0e740cc3e5999e5cc689a9
rokussadee <rokus.sadee@student.ehb.be>
2024-02-27 16:51:11 +0100

    feat(chatbot-models-crud): user model & save() function
    models/user.py:
    - User model class
    - save(): save the model to MongoDB database

--- models/user.py
+++ models/user.py
@@ -1,6 +1,12 @@
 from datetime import datetime
+from pymongo import collection, MongoClient, errors
+from bson.objectid import ObjectId
 import uuid
+import os
+from dotenv import load_dotenv
 import utils.helpers as helpers
+
+load_dotenv()
 
 
 def validate_email(email: str) -> str:
@@ -17,6 +23,20 @@ def validate_email(email: str) -> str:
 class User:
     def __init__(self,
                  uid: uuid.UUID = uuid.uuid4(),
-                 email: str = None):
+                 email: str = None,
+                 password: str = None):
         self.uid: uuid.UUID = uid
+        self.created_at: datetime = datetime.now()
         self.email: str = validate_email(email)
+        self.password: bytes = validate_password(password)
+
+    def save(self, client: MongoClient) -> ObjectId:
+        try:
+            db = client[os.getenv("DATABASE")]
+            col = db["users"]
+            query_result = col.insert_one(self.__dict__)
+            print(query_result)
+            inserted_id = query_result.inserted_id
+            return inserted_id
+        except Exception as e:
+            raise errors.PyMongoError(str(e))
