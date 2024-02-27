0b171eeec6b6efb3033e767635cf81051d1980c9
rokussadee <rokus.sadee@student.ehb.be>
2024-02-27 16:53:56 +0100

    docs(chatbot-models-crud): documentation for user model

--- models/user.py
+++ models/user.py
@@ -13,6 +13,8 @@ def validate_email(email: str) -> str:
     """
     Validates the email address using the helper function.
     Raises a ValueError if the email is not valid.
+    :param email: The email address to validate
+    :return: The validated email address
     """
     try:
         return helpers.validate_email_address(email)
@@ -20,17 +22,46 @@ def validate_email(email: str) -> str:
         raise ValueError(f"Invalid email address: {str(e)}")
 
 
+def validate_password(password: str) -> bytes:
+    """
+    Validates the password using the helper function.
+    Raises a ValueError if the password is not valid.
+
+    :param password: The password to validate.
+    :return: The validated password encrypted using a helper function.
+    """
+    try:
+        validated_password = helpers.validate_password(password)
+        return helpers.encrypt_password(validated_password)
+    except ValueError as e:
+        raise ValueError(f"Invalid password: {str(e)}")
+
+
 class User:
     def __init__(self,
                  uid: uuid.UUID = uuid.uuid4(),
                  email: str = None,
                  password: str = None):
+        """
+        Represents a user
+
+        :param uid: The UUID of the user.
+        :param email: The email address of the user.
+        :param password: A sequence of bytes representing the password of the user.
+        """
         self.uid: uuid.UUID = uid
         self.created_at: datetime = datetime.now()
         self.email: str = validate_email(email)
         self.password: bytes = validate_password(password)
 
     def save(self, client: MongoClient) -> ObjectId:
+        """
+        Saves the user into the MongoDB database.
+        Raises a PyMongoError if the record could not be inserted into the database.
+
+        :param client: The MongoDB client.
+        :return: The inserted record as a dict.
+        """
         try:
             db = client[os.getenv("DATABASE")]
             col = db["users"]
