929f61537732447658bb343a3fbca66a72bb3de4
rokussadee <rokus.sadee@student.ehb.be>
2024-02-27 17:22:00 +0100

    feat(chatbot-models-crud): conversation model & save() functionmodels/user.py:
    - Conversation model class
    - save(): save the model to MongoDB database

--- models/conversation.py
+++ models/conversation.py
@@ -1,12 +1,16 @@
 from datetime import datetime
 from typing import List
+from pymongo import MongoClient, errors
+import os
+from bson.objectid import ObjectId
 import uuid
 
 default_topic = ["New conversation topic"]
 
 
 class Conversation:
-    def __init__(self, user_uid: uuid.UUID,
+    def __init__(self,
+                 user_uid: str,
                  user_message_count: int = 0,
                  user_login_count: int = 0,
                  conversation_topics: List[str] = None):
@@ -19,7 +23,18 @@ class Conversation:
         :param conversation_topics: List of conversation topics.
         """
         self.created_at: datetime = datetime.now()
-        self.user_uid: uuid.UUID = user_uid
+        self.user_uid: uuid.UUID = uuid.UUID(user_uid)
         self.user_message_count: int = user_message_count
         self.user_login_count: int = user_login_count
         self.conversation_topics: List[str] = conversation_topics if conversation_topics is not None else default_topic
+
+    def save(self, client: MongoClient) -> ObjectId:
+        try:
+            db = client[os.getenv("DATABASE")]
+            col = db["conversations"]
+            query_result = col.insert_one(self.__dict__)
+            print(query_result)
+            inserted_id = query_result.inserted_id
+            return inserted_id
+        except Exception as e:
+            raise errors.PyMongoError(str(e))
