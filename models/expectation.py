13b13c00a7481c349bd52f1806d17d21994c5828
rokussadee <rokus.sadee@student.ehb.be>
2024-02-29 13:39:56 +0100

    docs(models-expectation): documentation for Expectation model

--- models/expectation.py
+++ models/expectation.py
@@ -5,14 +5,29 @@ from pymongo import MongoClient, errors
 
 class Expectation:
     def __init__(self,
+
                  reasoning: str,
                  user_predictions: List[str],
                  additional_data: List[str]):
+        """
+        Represents an expectation object.
+
+        :param reasoning: Reasoning about the user’s internal mental state.
+        :param user_predictions: Likely possibilities for the next user input.
+        :param additional_data: A list of any additional data that would be useful to improve the prediction.
+        """
         self.reasoning: str = reasoning
         self.user_predictions: List[str] = user_predictions
         self.additional_data: List[str] = additional_data
 
     def save(self, client: MongoClient) -> ObjectId:
+        """
+        Saves a created Expectation instance into the MongoDB database.
+        Raises a PyMongoError if the record could not be inserted into the database.
+
+        :param client: The MongoDB client.
+        :return: The MongoDB ObjectId of the saved expectation.
+        """
         try:
             db = client[os.getenv("DATABASE")]
             col = db["expectations"]
