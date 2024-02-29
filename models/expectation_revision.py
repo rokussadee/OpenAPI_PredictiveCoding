037e1fffc59e8ad3aad7e90ad3eece1328c03363
rokussadee <rokus.sadee@student.ehb.be>
2024-02-29 13:54:39 +0100

    docs(models-expectation_revision): docs for ExpectationRevision model

--- models/expectation_revision.py
+++ models/expectation_revision.py
@@ -8,11 +8,25 @@ class ExpectationRevision:
                  revised_input_possibilities: List[str],
                  prediction_error: float,
                  initial_expectation_id: ObjectId):
+        """
+        Represents an expectation revision object.
+
+        :param revised_input_possibilities: The revised input possibilities.
+        :param prediction_error: The prediction error.
+        :param initial_expectation_id: The ObjectId of the initial expectation object.
+        """
         self.revised_input_possibilities: List[str] = revised_input_possibilities
         self.prediction_error: float= prediction_error
         self.initial_expectation_id: ObjectId = initial_expectation_id
 
     def save(self, client: MongoClient) -> ObjectId:
+        """
+        Saves a created ExpectationRevision instance into the MongoDB database.
+        Raises a PyMongoError if the record could not be inserted into the database.
+
+        :param client: The MongoDB client.
+        :return: The MongoDB ObjectId of the saved expectation revision.
+        """
         try:
             db = client[os.getenv("DATABASE")]
             col = db["expectation_revisions"]
