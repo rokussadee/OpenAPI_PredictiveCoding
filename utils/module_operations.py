67d58b6cfbfa03919af6f67a0c0d688cc142851c
rokussadee <rokus.sadee@student.ehb.be>
2024-03-04 13:50:11 +0100

    refactor(fn-paradigm): module_operations; return types

--- utils/module_operations.py
+++ utils/module_operations.py
@@ -48,52 +48,70 @@ def create_model(collection_name: str, model_data: dict, client: MongoClient) ->
         raise errors.PyMongoError(str(e))
 
 
-def get_model(collection, model_id, client: MongoClient):
+def get_model(collection_name: str, model_id: ObjectId, client: MongoClient) -> dict:
     """
     Retrieves a model instance from the specified collection in the MongoDB database.
 
-    :param collection: The name of the collection from which to retrieve the model.
+    :param collection_name: The name of the collection from which to retrieve the model.
     :param model_id: The ObjectId of the model to retrieve.
     :param client: The MongoDB client.
     :return: A dictionary representing the retrieved model instance.
     """
+
+    if collection_name not in COLLECTION_DATA_CLASS_MAP:
+        raise ValueError(f"No data class defined for collection: {collection_name}")
+
+    data_class = COLLECTION_DATA_CLASS_MAP[collection_name]
     try:
         db = client[os.getenv("DATABASE")]
-        col = db[collection]
+        col = db[collection_name]
         model = col.find_one({"_id": ObjectId(model_id)})
+        if not isinstance(model.__dict__, data_class):
+            raise ValueError(f"model_data must be of type {data_class.__name__}")
+
         return model
     except Exception as e:
         raise errors.PyMongoError(str(e))
 
 
-def update_model(collection, model_id, update_data, client: MongoClient):
+def update_model(collection_name: str, model_id: ObjectId, update_data: dict, client: MongoClient) -> ObjectId:
     """
     Updates a model instance in the specified collection in the MongoDB database.
 
-    :param collection: The name of the collection containing the model to update.
+    :param collection_name: The name of the collection containing the model to update.
     :param model_id: The ObjectId of the model to update.
     :param update_data: A dictionary containing the updated data for the model.
     :param client: The MongoDB client.
     """
+
+    if collection_name not in COLLECTION_DATA_CLASS_MAP:
+        raise ValueError(f"No data class defined for collection: {collection_name}")
+
+    data_class = COLLECTION_DATA_CLASS_MAP[collection_name]
+    if not isinstance(update_data, data_class):
+        raise ValueError(f"model_data must be of type {data_class.__name__}")
+
     try:
         db = client[os.getenv("DATABASE")]
-        col = db[collection]
-        col.update_one({"_id": ObjectId(model_id)}, {"$set": update_data})
+        col = db[collection_name]
+        query_result = col.update_one({"_id": ObjectId(model_id)}, {"$set": update_data})
+        return query_result.upserted_id
     except Exception as e:
         raise errors.PyMongoError(str(e))
 
 
-def delete_model(collection, model_id, client: MongoClient):
+def delete_model(collection_name: str, model_id: ObjectId, client: MongoClient) -> int:
     """
     Deletes a model instance from the specified collection in the MongoDB database.
 
-    :param collection: The name of the collection containing the model to delete.
+    :param collection_name: The name of the collection containing the model to delete.
     :param model_id: The ObjectId of the model to delete.
     :param client: The MongoDB client.
     """
     try:
         db = client[os.getenv("DATABASE")]
-        col = db[collection]
-        col.delete_one({"_id": ObjectId(model_id)})
+        col = db[collection_name]
+        query_result = col.delete_one({"_id": ObjectId(model_id)})
+        return query_result.deleted_count
     except Exception as e:
         raise errors.PyMongoError(str(e))
